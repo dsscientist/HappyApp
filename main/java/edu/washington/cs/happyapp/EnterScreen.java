89e0b28e81b0470a51d1d3e1d00e570de4bf52ae
MoSummers-dy <44329219+MoSummers-dy@users.noreply.github.com>
2018-10-21 09:21:22 -0700

    Add files via upload

---
+++ main/java/edu/washington/cs/happyapp/EnterScreen.java
@@ -0,0 +1,32 @@
+package edu.washington.cs.happyapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EnterScreen extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_screen);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openDailyQuotes();
+            }
+        });
+    }
+
+    public void openDailyQuotes(){
+        Intent intent = new Intent(this, DailyQuote.class);
+        startActivity(intent);
+    }
+}
